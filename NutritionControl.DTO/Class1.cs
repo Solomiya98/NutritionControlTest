999b77c89370898b4220c50ccf2bb0ec6923e16b
Solomiia <solomia.prymich@gmail.com>
2020-01-08 20:36:15 +0200

    Created skeleton

---
+++ NutritionControl.DTO/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NutritionControl.DTO
+{
+    public class Class1
+    {
+    }
+}
---
+++ NutritionControl.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NutritionControl.DataAccess
+{
+    public class Class1
+    {
+    }
+}
